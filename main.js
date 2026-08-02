62eee7ab58c2451b8468776fab5fb6bb62c6ad41
Jennifer <jenniferkrantz@hotmail.com>
2026-08-02 11:14:00 +0200

    Initialized repository

---
+++ main.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll("button");
+const inputField = document.getElementById("result");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+        const buttonsValue = buttons[i].textContent;
+        if (buttonsValue === "C") {
+            resetResult();
+        } else if (buttonsValue === "=") {
+            calcResult();
+        } else {
+            addValue(buttonsValue);
+        }
+    });
+}
+
+function addValue(buttonsValue) {
+    inputField.value += buttonsValue;
+}
+
+function resetResult() {
+    inputField.value = "";
+}
+
+function calcResult() {
+    inputField.value = eval(inputField.value);
+}
